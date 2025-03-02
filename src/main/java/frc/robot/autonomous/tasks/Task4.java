141e1a26dbfb4e3e86e113d555a23271c1c5a44f
Kaan <86714701+Feyds@users.noreply.github.com>
2025-03-02 05:12:24 +0300

    OTONOM

--- src/main/java/frc/robot/autonomous/tasks/Task4.java
+++ src/main/java/frc/robot/autonomous/tasks/Task4.java
@@ -6,13 +6,16 @@ import com.studica.frc.AHRS.NavXComType;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.Coral.Coral;
 import frc.robot.subsystems.Drive.DriveTrain;
 
 public class Task4 implements AutoTask {
 
     DriveTrain drive = DriveTrain.getInstance();
+    Coral coral = Coral.getInstance();
 
     AHRS navx = new AHRS(NavXComType.kMXP_SPI);
     PIDController turnController = new PIDController(0.02, 0.0001, 0.002);  //TODO: PID DEĞERLERİ AYARLANACAK
@@ -23,39 +26,26 @@ public class Task4 implements AutoTask {
     public Task4() {
         navx.reset();
         turnController.setTolerance(2.0);
-        distanceController.setTolerance(2.0);
+        distanceController.setTolerance(0.02);
     }
 
     @Override
     public Command getCommand() {
         return new SequentialCommandGroup(
             new InstantCommand(() -> {
-                angleSetpoint = 90;
-                turnController.setSetpoint(angleSetpoint);
-
-                distanceSetpoint = 50;
+                distanceSetpoint = 0.88;
                 initYaw = navx.getYaw();
+                distanceController.setSetpoint(distanceSetpoint);
             }),
+            new RunCommand(() -> {
+                double speed = distanceController.calculate(navx.getDisplacementX());
+                drive.drive(speed, 0);
+            }, drive).withTimeout(3.0),
             new WaitCommand(0.5),
-            new InstantCommand(() -> {
-                while(!turnController.atSetpoint()) {
-                    double turnSpeed = turnController.calculate(navx.getAngle());
-                    drive.drive(0, turnSpeed);
-                    drive.updatePosition(0, turnSpeed);
-                }
-            }),
+            new InstantCommand(coral::score),
             new WaitCommand(0.5),
-            new InstantCommand(() -> {
-                while(!distanceController.atSetpoint()) {
-                    double speed = distanceController.calculate(navx.getDisplacementX());
-                    drive.drive(speed, 0);
-                    drive.updatePosition(speed, 0);
-                }
-            }),
-            new InstantCommand(() -> {
-                drive.drive(0, 0);
-                drive.updatePosition(0, 0);
-            })
+            new InstantCommand(coral::stop),
+            new InstantCommand(() -> drive.drive(0, 0))
         );
     }
 }
